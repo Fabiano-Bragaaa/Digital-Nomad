d1556fee846881cb285ed66372cec7513645cbdd
Fabiano Braga <fabianobragadev@gmail.com>
2025-08-13 22:25:50 -0300

    Expo Fonts Customizadas

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,12 +1,24 @@
 
-import { Box } from '@/src/components/Box';
+import { CityCard } from '@/src/components/CityCard';
+import { Screen } from '@/src/components/Screen';
 import { Text } from '@/src/components/Text';
+import { cityPreviewList } from '@/src/data/cities';
+import { FlatList, ListRenderItemInfo, Text as RNText } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+import { CityPreview } from '@/src/types';
 
 export default function HomeScreen() {
+
+  function renderItem({item}:ListRenderItemInfo<CityPreview>){
+    return <CityCard cityPreview={item}/>
+  }
+
   return (
-    <Box bg='mainBackground' flex={1}>
-      <Text color='text'>home screen</Text>
-    </Box>
+    <Screen>
+      <Text variant='title28'>barcelona</Text>
+      <RNText style={{color: '#fff', fontSize: 28}}>barcelona</RNText>
+    <FlatList data={cityPreviewList} keyExtractor={item => item.id} renderItem={renderItem}/>
+      </Screen>
   );
 }
 
---
+++ src/components/Screen.tsx
@@ -0,0 +1,10 @@
+import React, { PropsWithChildren } from "react";
+import { Box, BoxProps } from "./Box";
+
+export function Screen({children, ...boxProps}:PropsWithChildren & BoxProps) {
+  return (
+    <Box bg="background" paddingHorizontal="s16" {...boxProps}>
+      {children}
+    </Box>
+  )
+}
