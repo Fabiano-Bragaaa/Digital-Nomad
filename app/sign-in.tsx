087689600b689864f2a6a0395390c5eb2db362b1
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-08-28 21:28:23 -0300

    Tela de Sign-in

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,9 +1,10 @@
 import { useAuthSignIn } from "@/src/domain/auth/operations/useAuthSignIn";
 import { Button } from "@/src/ui/components/Button";
 import { Screen } from "@/src/ui/components/Screen";
+import { Text } from "@/src/ui/components/Text";
 import { TextInput } from "@/src/ui/components/TextInput";
 import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignInScreen() {
@@ -22,12 +23,24 @@ export default function SignInScreen() {
   return (
     <Screen>
       <SafeAreaView>
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={{
+            width: 150,
+            height: 60,
+            alignSelf: "center",
+            marginTop: 20,
+            marginBottom: 60,
+          }}
+        />
+        <Text variant="title22" alignSelf="center" mb="s16">
+          Bem-vindo
+        </Text>
         <TextInput
           label="Email"
           placeholder="Email"
           value={email}
           onChangeText={setEmail}
-          errorMessage="Email is required"
         />
         <TextInput
           label="Password"
@@ -36,20 +49,15 @@ export default function SignInScreen() {
           onChangeText={setPassword}
           secureTextEntry
         />
-        <Button onPress={handleSubmit} title="Sign in" />
-
+        <Text mb="s16" alignSelf="flex-end" variant="text14" color="primary">
+          Esqueceu sua senha
+        </Text>
+        <Button onPress={handleSubmit} title="Entrar" />
+        <Text alignSelf="center" mt="s16" color="gray2">
+          Ainda não tem sua conta?{" "} <Text variant="title14" color="primary">Criar</Text>
+        </Text>
       </SafeAreaView>
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({
-  input: {
-    borderColor: "#fff",
-    borderWidth: 1,
-    height: 60,
-    color: "#fff",
-    fontSize: 20,
-    marginVertical: 16,
-  },
-});
