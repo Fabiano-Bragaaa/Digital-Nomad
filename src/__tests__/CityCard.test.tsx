042ed63af95fc46aa6bd6c8dbffec8b39168b698
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-09-14 19:49:10 -0300

    Teste de Snapshot

--- src/__tests__/CityCard.test.tsx
+++ src/__tests__/CityCard.test.tsx
@@ -3,6 +3,16 @@ import { renderComponent } from "../test-utils/renderComponent"
 import { CityCard } from "../ui/components/CityCard"
 
 describe("<CityCard />", () => {
+  it('should render the component', () => {
+    renderComponent(<CityCard cityPreview={{
+      id: '1',
+      name: 'City 1',
+      country: 'Country 1',
+      coverImage: 'https://via.placeholder.com/150',
+    }}/>)
+
+    expect(screen.toJSON()).toMatchSnapshot()
+  })
   it('should display the city country', () => {
     renderComponent(<CityCard cityPreview={{
       id: '1',
