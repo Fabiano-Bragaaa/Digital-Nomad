eafb5a82d6e9f3be3a5eb9446d3c229e4779ebd4
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-09-19 07:25:01 -0300

    Limpando Cache no Sign Out

--- src/domain/auth/operations/useAuthSignOut.ts
+++ src/domain/auth/operations/useAuthSignOut.ts
@@ -1,6 +1,7 @@
 import { useFeedbackService } from "@/src/infra/feedbackService/FeedbackProvider";
 import { useAppMutation } from "@/src/infra/operations/useAppMutation";
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
+import { useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../AuthContext";
 
 export function useAuthSignOut() {
@@ -8,9 +9,12 @@ export function useAuthSignOut() {
   const {send} = useFeedbackService()
   const {auth} = useRepository()
 
+  const queryClient = useQueryClient()
+
   return useAppMutation({
     mutateFn: () => auth.signOut(),
     onSuccess: () => {
+      queryClient.clear()
       removeAuthUser()
     },
     onError: () => send({type: "error", message: "Erro ao realizar logout"}),
